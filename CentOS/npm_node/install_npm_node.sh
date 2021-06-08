601ed37caebb888812a431eb73ff2b94a91df3ff
thiodene-os <60824793+thiodene-os@users.noreply.github.com>
2021-06-08 17:26:30 -0400

    Create install_npm_node.sh

---
+++ CentOS/npm_node/install_npm_node.sh
@@ -0,0 +1,7 @@
+# Latest Version Install:
+
+## From the source get the library
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
+
+## Run `sudo yum install -y nodejs` to install Node.js 14.x and npm.
+sudo yum install -y nodejs
